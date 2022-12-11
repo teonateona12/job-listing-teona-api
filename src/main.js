14812d77c1a3b7d4c68ad479abf09fc8a5ec5c3a
Teona Piranishvili <teona.piranishvili1@gmail.com>
2022-12-11 14:03:50 +0400

    chore

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import express from "express";
-import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
+import Data from "./dataModel.js";
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send(data);
+app.use(cors());
+dotenv.config();
+
+mongoose.connect(process.env.CONNECTION_URL);
+app.get("/", async (req, res) => {
+  const data = await Data.find();
+  return res.send(data);
 });
 
 app.listen(5000);
